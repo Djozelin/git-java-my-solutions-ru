858877af52c581950dc6090341a9e6af41074fd3
djozelin <djozelin@mail.ru>
2017-01-06 01:06:16 +0300

    Изучаем японский

---
+++ level03/lesson06/task05/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level03.lesson06.task05;
+
+/* Изучаем японский
+Выведи на экран 日本語
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.print('\u65E5');
+        System.out.print('\u672C');
+        System.out.print('\u8A9E');
+    }
+}

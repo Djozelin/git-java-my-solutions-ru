03358b457a1435b0cda9b9e1f3c51d95be74247f
djozelin <djozelin@mail.ru>
2017-01-06 01:21:29 +0300

    День недели Ввести с клавиатуры номер дня недели, в зависимости от номера вывести название «понедельник», «вторник», «среда», «четверг», «пятница», «суббота», «воскресенье», если введен номер больше или меньше 7 – вывести «такого дня недели не существует». Пример для номера 5: пятница Пример для номера 10: такого дня недели не существует

---
+++ level04/lesson04/task06/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level04.lesson04.task06;
+
+/* День недели
+Ввести с клавиатуры номер дня недели, в зависимости от номера вывести название «понедельник», «вторник», «среда», «четверг», «пятница», «суббота», «воскресенье»,
+если введен номер больше или меньше 7 – вывести «такого дня недели не существует».
+Пример для номера 5:
+пятница
+Пример для номера 10:
+такого дня недели не существует
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+       InputStream inputStream=System.in;
+        Reader InputStreamReader=new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader (InputStreamReader);
+        String ns=bufferedReader.readLine();
+        int n= Integer.parseInt(ns);
+        if (n==1)
+                System.out.println("понедельник");
+        if (n==2)
+            System.out.println("вторник");
+        if (n==3)
+            System.out.println("среда");
+        if (n==4)
+            System.out.println("четверг");
+        if (n==5)
+            System.out.println("пятница");
+        if (n==6)
+            System.out.println("суббота");
+        if (n==7)
+            System.out.println("воскресенье");
+        if (n<1 || n>7)
+            System.out.println("такого дня недели не существует");//напишите тут ваш код
+
+    }
+
+}
